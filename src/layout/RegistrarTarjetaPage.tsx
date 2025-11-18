00f1b4cf673fec0b1648f7471292ed48f3f436cc
Jesús Manuel Martinez <jmanuelmartinez2003@gmail.com>
2025-11-18 04:11:56 -0300

    done

--- src/layout/RegistrarTarjetaPage.tsx
+++ src/layout/RegistrarTarjetaPage.tsx
@@ -6,7 +6,6 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import type { MoradorData, RegistrarTarjetaPageProps } from '../interface/morador.interface';
 import { API_ROUTES } from '../apiConfig';
-import { BarProgress } from '../shared/components/BarProgress';
 
 
 
@@ -116,7 +115,7 @@ export function RegistrarTarjetaPage({ onVolver }: RegistrarTarjetaPageProps) {
           </div>
 
           <ProgressBar step={step} />
-          
+
           {step === 1 && (
             <div className="rounded-xl bg-white p-8 shadow-lg animate-fade-in">
               <form onSubmit={handlePaso1Submit}>
