a624d355a8cb97d05eb592c72550183f0a1bb4fa
Jesús Manuel Martinez <jmanuelmartinez2003@gmail.com>
2025-11-18 01:41:38 -0300

    error de registro visitas

--- src/layout/RegistrarVisitaPage.tsx
+++ src/layout/RegistrarVisitaPage.tsx
@@ -23,7 +23,7 @@ export function RegistrarVisitaPage({ onVolver }: Props) {
     setError(null);
 
     try {
-      const response = await fetch(`${API_ROUTES.noAutorizados}/${dni}`,{
+      const response = await fetch(`${API_ROUTES.noAutorizados}/${dni}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -57,7 +57,7 @@ export function RegistrarVisitaPage({ onVolver }: Props) {
     setError(null);
 
     try {
-      const response = await fetch(`http://localhost:4000/api/visitas`, {
+      const response = await fetch(`${API_ROUTES.visitas}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
